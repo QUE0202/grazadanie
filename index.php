292bf35c00f7287f77c2a268176855c41ff9cedb
que2115 <jakub1016@gmail.com>
2020-12-07 16:31:21 +0100

    update

--- index.php
+++ index.php
@@ -44,6 +44,52 @@
         
         
     ?>
+<?php 
+        require('./class/Village.class.php');
+        session_start();
+        if(!isset($_SESSION['v'])) // jeżeli nie ma w sesji naszej wioski
+        {
+            echo "Tworzę nową wioskę...";
+            $v = new Village();
+            $_SESSION['v'] = $v;
+            //reset czasu od ostatniego odświerzenia strony
+            $deltaTime = 0;
+        } 
+        else //mamy już wioskę w sesji - przywróć ją
+        {
+            $v = $_SESSION['v'];
+            //ilosc sekund od ostatniego odświerzenia strony
+            $deltaTime = time() - $_SESSION['time'];
+        }
+        $v->gain($deltaTime);
+        
+        if(isset($_REQUEST['action'])) 
+        {
+            switch($_REQUEST['action'])
+            {
+                case 'upgradeBuilding':
+                    if($v->upgradeBuilding($_REQUEST['building']))
+                    {
+                        echo "Ulepszono budynek: ".$_REQUEST['building'];
+                    }
+                    else
+                    {
+                        echo "Nie udało się ulepszyć budynku: ".$_REQUEST['building'];
+                    }
+                    
+                break;
+                default:
+                    echo 'Nieprawidłowa zmienna "action"';
+            }
+        }
+
+
+
+
+        $_SESSION['time'] = time();
+        
+        
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -83,14 +129,23 @@
                 Lista budynków<br>
                 Drwal, poziom <?php echo $v->buildingLVL("woodcutter"); ?> <br>
                 Zysk/h: <?php echo $v->showHourGain("wood"); ?><br>
+                <?php if($v->checkBuildingUpgrade("woodcutter")) : ?>
                 <a href="index.php?action=upgradeBuilding&building=woodcutter">
                     <button>Rozbuduj drwala</button>
                 </a><br>
+                <?php else : ?>
+                    <button onclick="missingResourcesPopup()">Rozbuduj drwala</button><br>
+                <?php endif; ?>
                 Kopalnia żelaza, poziom <?php echo $v->buildingLVL("ironMine"); ?> <br>
                 Zysk/h: <?php echo $v->showHourGain("iron"); ?><br>
+                <?php if($v->checkBuildingUpgrade("ironMine")) : ?>
                 <a href="index.php?action=upgradeBuilding&building=ironMine">
                     <button>Rozbuduj kopalnie żelaza</button>
                 </a>
+                <?php else : ?>
+                    <button onclick="missingResourcesPopup()">Rozbuduj kopalnie żelaza</button>
+                <br>
+                <?php endif; ?>
             </div>
             <div class="col-12 col-md-6">
                 Widok wioski
@@ -110,7 +165,11 @@
             </div>
         </footer>
     </div>
-
+    <script>
+        function missingResourcesPopup() {
+            window.alert("Brakuje zasobów");
+        }
+    </script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
